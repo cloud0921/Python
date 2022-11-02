af0102064f58b41f441b6b8a6f9696f02d5474ab
cloud0921 <ssjjhhcc@gmail.com>
2022-11-02 18:42:40 +0900

    commit

--- Practice/환율변환기/main7-3.py
+++ Practice/환율변환기/main7-3.py
@@ -8,8 +8,6 @@ def get_exchange_rate(target1, target2):
     }
     response =requests.get("https://kr.investing.com/currencies/{}-{}".format(target1,target2),headers=headers)
     content =BeautifulSoup(response.content, 'html.parser')
-    containers = content.find('span',{'data-test':'instrument-price-last'})
-    print(content)
+    containers = content.find_all("span",{"data-test":"instrument-price-last"})
     print(containers.text)
-
 get_exchange_rate('usd','krw')
